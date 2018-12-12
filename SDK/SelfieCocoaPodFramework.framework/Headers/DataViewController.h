b60ccd38178376693abd8facb362519bb874772a
IDM014 <IDM014@IDM012s-Mac-mini.local>
2018-12-12 15:42:55 +0530

    chnages

---
+++ SDK/SelfieCocoaPodFramework.framework/Headers/DataViewController.h
@@ -0,0 +1,18 @@
+//
+//  DataViewController.h
+//  SelfieCocoaPodFramework
+//
+//  Created by IDM014 on 12/12/18.
+//  Copyright © 2018 e. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+
+@interface DataViewController : UIViewController
+
+-(void)callKrishna;
+
+@end
+
---
+++ SDK/SelfieCocoaPodFramework.framework/Headers/SelfieCocoaPodFramework.h
@@ -0,0 +1,19 @@
+//
+//  SelfieCocoaPodFramework.h
+//  SelfieCocoaPodFramework
+//
+//  Created by IDM014 on 12/12/18.
+//  Copyright © 2018 e. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SelfieCocoaPodFramework/DataViewController.h>
+//! Project version number for SelfieCocoaPodFramework.
+FOUNDATION_EXPORT double SelfieCocoaPodFrameworkVersionNumber;
+
+//! Project version string for SelfieCocoaPodFramework.
+FOUNDATION_EXPORT const unsigned char SelfieCocoaPodFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SelfieCocoaPodFramework/PublicHeader.h>
+
+
