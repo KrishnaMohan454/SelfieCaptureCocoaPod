937fbaae924803e0db29a4eeeb5fae215305a72f
IDM014 <IDM014@IDM012s-Mac-mini.local>
2018-12-12 15:15:49 +0530

    changes

--- SDK/SelfieCaptureCocoaPods.framework/Headers/MessageViewController.h
+++ SDK/SelfieCaptureCocoaPods.framework/Headers/MessageViewController.h
@@ -9,7 +9,6 @@
 #import <UIKit/UIKit.h>
 #import <DocumentCaptureCocoaPods/DocumentCaptureCocoaPods.h>
 
-NS_ASSUME_NONNULL_BEGIN
 
 @interface MessageViewController : UIViewController
 
@@ -17,4 +16,3 @@ NS_ASSUME_NONNULL_BEGIN
 
 @end
 
-NS_ASSUME_NONNULL_END
